1ec42babd29e01f5e5081d54dbe8757b040f5dea
lalosalamanca1964 <142697060+lalosalamanca1964@users.noreply.github.com>
2024-01-17 14:41:11 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,388 @@
+"use strict";
+
+let burgerBtn = document.getElementById('burger');
+
+const dropdownContent = document.querySelector("#burger-container");
+
+document.addEventListener("click", function (event) {
+    if (!burgerBtn.contains(event.target) && !dropdownContent.contains(event.target)) {
+      dropdownContent.style.display = "none";
+    }
+});
+
+burgerBtn.addEventListener("click", function () {
+    dropdownContent.style.display = (dropdownContent.style.display === "flex") ? "none" : "flex";
+});
+
+let itemsOrdArr;
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.body.classList.add("loaded");
+    itemsOrdArr = Array.from(Object.keys(items));
+});
+
+const items =  {
+    agent: {
+        value: 2500000,
+        dupedValue: 'N/A',
+        demand: 'Low',
+        status: 'Stable',
+        monthlyUnique: 'Rare'
+    },
+    airtail: {
+        value: 1500000,
+        dupedValue: 'N/A',
+        demand: 'Medium',
+        status: 'Stable',
+        monthlyUnique: 'Common'
+    },
+    aperture: {
+        value: 2500000,
+        dupedValue: 'N/A',
+        demand: 'Medium',
+        status: 'Stable',
+        monthlyUnique: 'Uncommon'
+    },
+    arach: {
+        value: 25000000,
+        dupedValue: 17500000,
+        demand: 'Low',
+        status: 'Stable',
+        monthlyUnique: 'Extremely rare'
+    },
+    banana: {
+        value: 22500000,
+        dupedValue: 'N/A',
+        demand: 'Very High',
+        status: 'Stable',
+        monthlyUnique: 'Extremely rare'
+    },
+    bandit: {
+        value: 2500000,
+        dupedValue: 'N/A',
+        demand: 'Medium',
+        status: 'Stable',
+        monthlyUnique: 'Uncommon'
+    },
+    beam: {
+        value: 35000000,
+        dupedValue: 25000000,
+        demand: 'Low',
+        status: 'Dropping',
+        monthlyUnique: 'Extremely rare'
+    },
+    beig: {
+        value: 20000000,
+        dupedValue: 20000000,
+        demand: 'Low',
+        status: 'Stable',
+        monthlyUnique: 'Rare'
+    },
+    hawk: {
+        value: 850000,
+        dupedValue: 'N/A',
+        demand: 'Low',
+        status: 'Stable',
+        monthlyUnique: 'Common'
+    },
+    bloxy: {
+        value: 1250000,
+        dupedValue: 'N/A',
+        demand: 'Medium',
+        status: 'Stable',
+        monthlyUnique: 'Uncommon'
+    },
+    brulee: {
+        value: 8000000,
+        dupedValue: 6500000,
+        demand: 'Low',
+        status: 'Stable',
+        monthlyUnique: 'Common'
+    },
+    cel: {
+        value: 22500000,
+        dupedValue: 20000000,
+        demand: 'Very High',
+        status: 'Stable',
+        monthlyUnique: 'Very rare'
+    },
+    classic: {
+        value: 200000,
+        dupedValue: 'N/A',
+        demand: 'Very Low',
+        status: 'Stable',
+        monthlyUnique: 'Common'
+    },
+    concept: {
+        value: 2500000,
+        dupedValue: 'N/A',
+        demand: 'Low',
+        status: 'Stable',
+        monthlyUnique: 'Common'
+    },
+    crew: {
+        value: 12500000,
+        dupedValue: 'N/A',
+        demand: 'Low',
+        status: 'Stable',
+        monthlyUnique: 'Extremely rare'
+    },
+    drone: {
+        value: 850000,
+        dupedValue: 'N/A',
+        demand: 'Low',
+        status: 'Stable',
+        monthlyUnique: 'Common'
+    },
+    escape: {
+        value: 200000,
+        dupedValue: 'N/A',
+        demand: 'Very Low',
+        status: 'Stable',
+        monthlyUnique: 'Common'
+    },
+    fiasco: {
+        value: 100000,
+        dupedValue: 'N/A',
+        demand: 'Very Low',
+        status: 'Stable',
+        monthlyUnique: 'Uncommon'
+    },
+    goliath: {
+        value: 2500000,
+        dupedValue: 'N/A',
+        demand: 'Medium',
+        status: 'Stable',
+        monthlyUnique: 'Uncommon'
+    },
+    ice: {
+        value: 30000000,
+        dupedValue: 25000000,
+        demand: 'Low',
+        status: 'Dropping',
+        monthlyUnique: 'Extremely rare'
+    },
+    jack: {
+        value: 850000,
+        dupedValue: 'N/A',
+        demand: 'Lower',
+        status: 'Stable',
+        monthlyUnique: 'rare'
+    },
+    jav: {
+        value: 12500000,
+        dupedValue: 10000000,
+        demand: 'High',
+        status: 'Stable',
+        monthlyUnique: 'Uncommon'
+    },
+    jb8: {
+        value: 7500000,
+        dupedValue: 'N/A',
+        demand: 'Low',
+        status: 'Stable',
+        monthlyUnique: 'Rare'
+    },
+    laviolette: {
+        value: 250000,
+        dupedValue: 'N/A',
+        demand: 'Low',
+        status: 'Stable',
+        monthlyUnique: 'rare'
+    },
+    mac: {
+        value: 4000000,
+        dupedValue: 'N/A',
+        demand: 'High',
+        status: 'Stable',
+        monthlyUnique: 'Uncommon'
+    },
+    megalodon: {
+        value: 850000,
+        dupedValue: 'N/A',
+        demand: 'Lower',
+        status: 'Stable',
+        monthlyUnique: 'Common'
+    },
+    mighty: {
+        value: 150000,
+        dupedValue: 'N/A',
+        demand: 'Very Low',
+        status: 'Stable',
+        monthlyUnique: 'Common'
+    },
+    m12: {
+        value: 5500000,
+        dupedValue: 'N/A',
+        demand: 'High',
+        status: 'Stable',
+        monthlyUnique: 'Uncommon'
+    },
+    nascar: {
+        value: 100000,
+        dupedValue: 'N/A',
+        demand: 'Very Low',
+        status: 'Stable',
+        monthlyUnique: 'Common'
+    },
+    parisian: {
+        value: 3250000,
+        dupedValue: 'N/A',
+        demand: 'High',
+        status: 'Stable',
+        monthlyUnique: 'Common'
+    },
+    poseidon: {
+        value: 2500000,
+        dupedValue: 'N/A',
+        demand: 'Low',
+        status: 'Stable',
+        monthlyUnique: 'Uncommon'
+    },
+    posh: {
+        value: 400000,
+        dupedValue: 'N/A',
+        demand: 'Very Low',
+        status: 'Stable',
+        monthlyUnique: 'Common'
+    },
+    power1: {
+        value: 5500000,
+        dupedValue: 'N/A',
+        demand: 'Very High',
+        status: 'Stable',
+        monthlyUnique: 'Rare'
+    },
+    proto: {
+        value: 12500000,
+        dupedValue: 10000000,
+        demand: 'High',
+        status: 'Stable',
+        monthlyUnique: 'Uncommon'
+    },
+    raptor: {
+        value: 15000000,
+        dupedValue: 'N/A',
+        demand: 'Medium',
+        status: 'Stable',
+        monthlyUnique: 'Extremely rare'
+    },
+    rattler: {
+        value: 2500000,
+        dupedValue: 'N/A',
+        demand: 'Medium',
+        status: 'Stable',
+        monthlyUnique: 'Very rare'
+    },
+    shell: {
+        value: 850000,
+        dupedValue: 'N/A',
+        demand: 'Lower',
+        status: 'Stable',
+        monthlyUnique: 'Rare'
+    },
+    shogun: {
+        value: 3000000,
+        dupedValue: 'N/A',
+        demand: 'High',
+        status: 'Stable',
+        monthlyUnique: 'Common'
+    },
+    snake: {
+        value: 1500000,
+        dupedValue: 'N/A',
+        demand: 'Medium',
+        status: 'Stable',
+        monthlyUnique: 'Common'
+    },
+    steed: {
+        value: 250000,
+        dupedValue: 'N/A',
+        demand: 'Very Low',
+        status: 'Stable',
+        monthlyUnique: 'Uncommon'
+    },
+    suv: {
+        value: 350000,
+        dupedValue: 'N/A',
+        demand: 'Very Low',
+        status: 'Stable',
+        monthlyUnique: 'Common'
+    },
+    tiny: {
+        value: 1250000,
+        dupedValue: 'N/A',
+        demand: 'Medium',
+        status: 'Stable',
+        monthlyUnique: 'Common'
+    },
+    torero: {
+        value: 2500000,
+        dupedValue: 'N/A',
+        demand: 'Medium',
+        status: 'Stable',
+        monthlyUnique: 'Rare'
+    },
+    torpedo: {
+        value: 35000000,
+        dupedValue: 30000000,
+        demand: 'Decent',
+        status: 'Rising',
+        monthlyUnique: 'Very rare'
+    },
+    volt4x4: {
+        value: 4000000,
+        dupedValue: 'N/A',
+        demand: 'Medium',
+        status: 'Stable',
+        monthlyUnique: 'Very rare'
+    },
+    volt: {
+        value: 850000,
+        dupedValue: 'N/A',
+        demand: 'Low',
+        status: 'Stable',
+        monthlyUnique: 'Common'
+    },
+};
+
+var valuesArr = [];
+
+function getValuesArr() {
+    const itemsValueArr = Object.values(items);
+    for (let i = 0; i < itemsValueArr.length; i++) {
+        const value = itemsValueArr[i].value;
+        valuesArr.push(value);
+    }
+    return valuesArr
+}
+
+getValuesArr();
+
+function displayItemInfo() {
+    var userInput = document.getElementById("user-input").value.toLowerCase();
+        if (/^[a-zA-Z0-9\-_]+$/.test(userInput)) {
+            for (let i = 0; i < itemsOrdArr.length; i++) {
+                if (userInput.includes(itemsOrdArr[i])) {
+                    var userInput = itemsOrdArr[i];
+                    document.getElementById('value-box').innerHTML = (`Value - ${items[userInput].value}`);
+                    document.getElementById('duped-box').innerHTML = (`Duped Value - ${items[userInput].dupedValue}`);
+                    document.getElementById('demand-box').innerHTML = (`Demand - ${items[userInput].demand}`);
+                    document.getElementById('status-box').innerHTML = (`Status - ${items[userInput].status}`);
+                    document.getElementById('unique-box').innerHTML = (`Rarity - ${items[userInput].monthlyUnique}`);
+                    document.getElementById('error-box').style.display = ('none');
+                    document.getElementById('display-item-image').style.display = ('block');
+                    document.getElementById('display-item-image').src = ('images/' + `${userInput}` + '.png');
+                    return
+                }
+                else {
+                    document.getElementById('error-box').style.display = ('block');
+                    document.getElementById('error-box').innerHTML = ('Invalid Input');
+                }
+            }
+        } else {
+        console.log('what u doin huh?')
+        document.getElementById('error-box').style.display = ('block');
+        document.getElementById('error-box').innerHTML = ('Invalid Input');
+        }        
+}
